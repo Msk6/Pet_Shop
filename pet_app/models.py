5314c882f399e4701a083e6e0181dc31c839cf8d
Msk6 <Maha.s.kh@hotmail.com>
2020-09-06 11:24:01 +0300

    Create model and preparing packages

--- pet_app/models.py
+++ pet_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+class Pet(models.Model):
+    name = models.CharField(max_length=250)
+    age = models.IntegerField()
+    available = models.BooleanField(default=True)
+    image = models.ImageField(blank=True, null=True)
+    price = models.FloatField()
+
+    def __str__(self):
+        return self.name
+
+    
